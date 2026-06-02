5244244c458d772d5747aa025c8e4a05ee1dcee1
TheMuno <143614176+TheMuno@users.noreply.github.com>
2026-06-02 13:19:21 +0300

    Update day-off by one error

--- itinerary-list.js
+++ itinerary-list.js
@@ -291,7 +291,7 @@ function processTitleDates(date) {
   const theDate = parseJSON(date);
   if (!theDate) return;
   const { dateStr, flatpickrDate } = theDate;
-  const dateToExtractFrom = dateStr ? dateStr : flatpickrDate;
+  const dateToExtractFrom = flatpickrDate ? flatpickrDate : dateStr;
   const [ startDate, endDate ] = dateToExtractFrom.split(/\s+to\s+/);
   return getTitleDates(startDate, endDate);
 }
