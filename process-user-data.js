d1a06a6df66c36f42b9636ecfc23315df561f414
TheMuno <143614176+TheMuno@users.noreply.github.com>
2026-07-09 14:31:52 +0300

    Process hotel data tracking

---
+++ process-user-data.js
@@ -0,0 +1,17 @@
+const MAKE_WEBHOOK_URL = 'https://hook.us1.make.com/z0fx4wnlhhmdemvkvyic15xkleyd02um';
+
+sendTrackingDataToMake();
+
+function sendTrackingDataToMake() {
+  const params = new URLSearchParams(window.location.search);
+  const ref = params.get('ref');
+  const conf = params.get('conf');
+
+  if (!ref && !conf) return;
+
+  fetch(MAKE_WEBHOOK_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ ref, conf }),
+  }).catch(err => console.error('Failed to send data to Make.com:', err));
+}
