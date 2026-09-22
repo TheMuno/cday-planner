b667f207f3a30b835cbea41f3d886fb737182d57
TheMuno <143614176+TheMuno@users.noreply.github.com>
2026-09-22 14:32:09 +0300

    Redirect to the correct hotel

--- flow-trial.js
+++ flow-trial.js
@@ -1,7 +1,40 @@
+const $hotel = document.querySelector('[data-ak="hotel-name"]');
+const $lastName = document.querySelector('[data-ak="last-name"]');
+const $travelDates = document.querySelector('[data-ak="user-travel-dates"]');
+const $submitBtn = document.querySelector('[data-ak="submit"]');
 
-const $submitBtn = document.querySelector('#submit-user-details');
+const hotelMap = {
+    'carlton': '/carlton-arms',
+    'compton': '/compton',
+    'demo': '/demo-hotel/trip-planner',
+};
+
+const defaultRedirect = '/demo-hotel/trip-planner';
+let redirect = defaultRedirect;
+
+$hotel.addEventListener('change', e => {
+    const val = $hotel.value.trim().toLowerCase();
+    if (val.includes('carlton')) {
+        redirect = hotelMap['carlton'];
+    }
+    else if (val.includes('compton')) {
+        redirect = hotelMap['compton'];
+    }
+    else {
+        redirect = defaultRedirect;
+    }
+
+    console.log('hotel', val)
+    console.log('redirect', redirect)
+});
+
+
+
+
 $submitBtn.addEventListener('click', e => {
     e.preventDefault();
 
+    window.location.href = redirect;
     console.log('Clicked!!')
 });
+
